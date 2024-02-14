190fb908520548a67f0cbef744449ed9d7a05f7b
Chukwuka (Chuks) Egbuchunam <39104359+chukse@users.noreply.github.com>
2024-02-14 11:04:57 -0600

    Add files via upload

---
+++ QB analysis/old sorted dataframe.py
@@ -0,0 +1,41 @@
+sorted_df_2020 = df_2020.sort_values(by = ['twp_rate', 'player_game_count', 'accuracy_percent', 'aimed_passes', 'attempts','big_time_throws',
+                                           'completions','dropbacks','grades_offense','grades_pass','grades_run','touchdowns','turnover_worthy_plays'], axis=0, 
+                           ascending=[True, False,False,False,False,False,False,False,False,False,False,False,False], 
+                           inplace=False,
+               kind='quicksort', na_position='first', 
+                           ignore_index=True, key=None)
+
+
+
+
+sorted_df_2021 = df_2021.sort_values(by = ['twp_rate', 'player_game_count', 'accuracy_percent', 'aimed_passes', 'attempts','big_time_throws',
+                                           'completions','dropbacks','grades_offense','grades_pass','grades_run','touchdowns','turnover_worthy_plays'], axis=0, 
+                           ascending=[True, False,False,False,False,False,False,False,False,False,False,False,False], 
+                           inplace=False,
+               kind='quicksort', na_position='first', 
+                           ignore_index=True, key=None)
+
+
+
+sorted_df_2022 = df_2022.sort_values(by = ['twp_rate','player_game_count', 'accuracy_percent', 'aimed_passes', 'attempts','big_time_throws',
+                                           'completions','dropbacks','grades_offense','grades_pass','grades_run','touchdowns','turnover_worthy_plays'], axis=0, 
+                           ascending=[True, False,False,False,False,False,False,False,False,False,False,False,False], 
+                           inplace=False,
+               kind='quicksort', na_position='first', 
+                           ignore_index=True, key=None)
+
+sorted_df_2023 = df_2023.sort_values(by = ['twp_rate','player_game_count', 'accuracy_percent', 'aimed_passes', 'attempts','big_time_throws',
+                                           'completions','dropbacks','grades_offense','grades_pass','grades_run','touchdowns','turnover_worthy_plays'], axis=0, 
+                           ascending=[True, False,False,False,False,False,False,False,False,False,False,False,False], 
+                           inplace=False,
+               kind='quicksort', na_position='first', 
+                           ignore_index=True, key=None)
+
+
+
+sorted_df_2024 = df_2024.sort_values(by = ['twp_rate', 'player_game_count', 'accuracy_percent', 'aimed_passes', 'attempts','big_time_throws',
+                                           'completions','dropbacks','grades_offense','grades_pass','grades_run','touchdowns','turnover_worthy_plays'], axis=0, 
+                           ascending=[True, False,False,False,False,False,False,False,False,False,False,False,False], 
+                           inplace=False,
+               kind='quicksort', na_position='first', 
+                           ignore_index=True, key=None)
